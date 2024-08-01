def51dccf090903ed88d68d8895fdab1930d3b58
0x13rian <0x13rian@gmail.com>
2024-08-02 00:24:18 +1000

    AI summary feature

--- org_analysis.py
+++ org_analysis.py
@@ -9,8 +9,13 @@ import logging
 import os
 from datetime import datetime
 import matplotlib.patheffects as path_effects
+from anthropic import Anthropic
 
 CONFIG = loadConfig()
+
+AI_client = Anthropic(
+    api_key=CONFIG["ANTHROPIC_API_KEY"]
+)
 
 logging.basicConfig(filename='org_analysis.log', filemode='w', level=logging.ERROR, format='%(asctime)s - %(levelname)s - %(message)s')
 logger = logging.getLogger(__name__)
@@ -36,7 +41,7 @@ REPO_FULL_URL = "https://api.github.com/repos/{org_name}/{repo_name}"
 REPO_COLLAB_URL = "https://api.github.com/repos/{org_name}/{repo_name}/collaborators"
 REPO_COMMITS_URL = "https://api.github.com/repos/{org_name}/{repo_name}/commits?page={page}&per_page={per_page}"
 # COMMIT_COLUMNS = {'login': [], 'avatar_url':[],  'type': [], 'date': [], 'isFork':[]}
-# REPO_COLUMNS = {'name':[], 'description': [], 'updated_at': [], 'created_at': [], 'size':[], 'stars': [], 'watchers': [],  'language':[], "issues":[], "license": [], "isFork": [], "forkOf": []}
+# REPO_COLUMNS = {'name':[], 'description': [], 'updated_at': [], 'created_at': [], 'size':[], 'stars': [], 'watchers': [],  'language':[], "issues":[], "license": [], "isFork": [], "forkOf": [], "AICommitSummary": []}
 
 def make_paged_request(url, num_entries, analysisConfig: OrgAnalysisConfig, repo_name=""):
   resp = []
@@ -80,16 +85,39 @@ def make_request(url):
 def pretty_json(j):
   logger.info(json.dumps(j, indent=2))
 
-def org_info(json):
+def org_info(json, analysisConfig: OrgAnalysisConfig):
   if json == None:
-    logger.info(f"No org found with the name {ANALYSIS_CONFIG.ORG_NAME}. Exiting...")
+    logger.info(f"No org found with the name {analysisConfig.ORG_NAME}. Exiting...")
     exit()
 
   logger.info(f"Name: {json.get('name')}")
+  logger.info(f"Description: {json.get('description')}")
   logger.info(f"Created at: {json.get('created_at')}")
   logger.info(f"Updated at: {json.get('updated_at')}")
   logger.info(f"Number of public repos: {json.get('public_repos')}")
   logger.info(f"Number of followers: {json.get('followers')}")
+  # pretty_json(json)
+  summary = f"""
+Organization summary
+Name: {json.get('name')}
+Description: {json.get('description')}
+Number of followers: {json.get('followers')}
+"""
+  return summary
+
+def truncate_to_token_limit(text, max_tokens):
+    tokens = 0
+    words = text.split()
+    truncated = []
+    
+    for word in words:
+        word_tokens = AI_client.count_tokens(word)
+        if tokens + word_tokens > max_tokens:
+            break
+        truncated.append(word)
+        tokens += word_tokens
+    
+    return ' '.join(truncated)
 
 def print_commit_info(i):
   pretty_json(i)
@@ -100,9 +128,10 @@ def print_commit_info(i):
   # logger.info(f"Message: {i.get('commit').get('message')}") # TODO: feed this and summarise with AI?
   # logger.info(f"Name: {i.get('committer').get('name')}")
 
-def commit_info(j, isFork):
+def commit_info(j, isFork, orgSummary:str, repoSummary: str):
   commit_data = {'login': [], 'avatar_url':[],  'type': [], 'date': [], 'isFork':[]}
-
+  commit_messages = ""
+  count = 1
   for i in j:
     # print_commit_info(i)
     if i.get('author') != None:
@@ -117,13 +146,46 @@ def commit_info(j, isFork):
       else:
         commit_data['avatar_url'].append(None)
         commit_data['type'].append(None)
-
+    
     commit_data['date'].append(i.get('commit').get('committer').get('date'))
     commit_data['isFork'].append(isFork)
 
+    if "chore" not in i.get('commit').get('message'):
+      commit_messages += f"""
+Date: {commit_data['date'][-1]}
+Message: {i.get('commit').get('message')}
+"""
+    count+=1
+
+  AI_info = f"""
+{orgSummary}
+{repoSummary}
+{commit_messages}
+"""
+
+  AI_info = truncate_to_token_limit(AI_info, 1024)
+  logger.info(AI_info)
+  logger.info(f"Input tokens: {AI_client.count_tokens(AI_info)}")
+
+  message = AI_client.messages.create(
+    max_tokens=1024,
+    system=f"You are a software expert working for an investment firm. You are given summaries of a Github organization, a repository, and a list of commits from that repository. Summarise the commit history in two sentences in general terms. Avoid mentioning the specifics of the changes, instead focusing on how the changes affect the repository. Focus on major changes or updates in the repository and ignore non-substantial changes (documentation changes, minor fixes, package tracker updates, etc.) Read between the lines. If there are no major changes or updates, mention this. Identify any potential risks or concerns in the repository in one sentence. Identify how the repository contriubtes to the organization's product in one sentence. Do not mention needed further evaluation or due dilligence needed as this is a given.",
+    messages=[
+      {
+        "role": "user",
+        "content": AI_info,
+      }
+    ],
+    model="claude-3-opus-20240229",
+  )
+  # logger.info(f"AI response: {message.content}")
+  logger.info(f"AI response: {message.content[0].text}")
+  logger.info(f"AI response tokens: {AI_client.count_tokens(message.content[0].text)}")
+  AI_summary = message.content[0].text
+
   df = pd.DataFrame(commit_data)
   df['date'] = pd.to_datetime(df['date']).dt.date
-  return df
+  return df, AI_summary
 
 def print_repo_info(i):
   # pretty_json(i)
@@ -196,14 +258,14 @@ def create_pie(countSlice, labelSlice, title, fileName, fileDir, pieChartThresho
   plt.title(title, pad=70, loc='center', fontweight='bold', fontsize=16)
   plt.savefig(f"{fileDir}{fileName}_{datetime.now().isoformat(timespec='seconds').replace(':', '-')}.png")
 
-def aggRepo(json, analysisConfig: OrgAnalysisConfig):
-  count = 10 # len(json) # used for testing to limit the number of repo's analysed
+def aggRepo(json, analysisConfig: OrgAnalysisConfig, orgSummary: str):
+  count = 2 # len(json) # used for testing to limit the number of repo's analysed
   commitStats = pd.DataFrame({'login': [], 'avatar_url':[],  'type': [], 'date': [], 'isFork':[]})
-  repoStats = pd.DataFrame({'name':[], 'description': [], 'updated_at': [], 'created_at': [], 'size':[], 'stars': [], 'watchers': [],  'language':[], "issues":[], "license": [], "isFork": [], "forkOf": []})
+  repoStats = pd.DataFrame({'name':[], 'description': [], 'updated_at': [], 'created_at': [], 'size':[], 'stars': [], 'watchers': [],  'language':[], "issues":[], "license": [], "isFork": [], "forkOf": [], "AICommitSummary": []})
 
   for i in json:
     # print_repo_info(i)
-    repoData = {'name':[], 'description': [], 'updated_at': [], 'created_at': [], 'size':[], 'stars': [], 'watchers': [],  'language':[], "issues":[], "license": [], "isFork": [], "forkOf": []}
+    repoData = {'name':[], 'description': [], 'updated_at': [], 'created_at': [], 'size':[], 'stars': [], 'watchers': [],  'language':[], "issues":[], "license": [], "isFork": [], "forkOf": [], "AICommitSummary": []}
     repoData['name'].append(i.get('name'))
     repoData['description'].append(i.get('description'))
     repoData['updated_at'].append(i.get('updated_at'))
@@ -214,20 +276,31 @@ def aggRepo(json, analysisConfig: OrgAnalysisConfig):
     repoData['language'].append(i.get('language'))
     repoData['issues'].append(i.get('open_issues_count'))
 
+    repoSummary = f"""
+Repository summary:
+Name: {i.get('name')}
+Description: {i.get('description')}
+"""
+
     if i.get('fork') == True:
-      # logger.info(f"Repo: {ANALYSIS_CONFIG.ANALYSIS_CONFIG.ORG_NAME}/{i.get('name')} is a fork. Checking parent...")
+      logger.info(f"Repo: {analysisConfig.ORG_NAME}/{i.get('name')} is a fork. Checking parent...")
       repoData['isFork'].append(True)
       full_repo_json = make_request(REPO_FULL_URL.format(org_name=analysisConfig.ORG_NAME, repo_name=i.get('name')))
       if full_repo_json:
         repoData["forkOf"].append(full_repo_json.get("parent").get("full_name"))
     else:
-      # logger.info(f"Repo: {ANALYSIS_CONFIG.ANALYSIS_CONFIG.ORG_NAME}/{i.get('name')} is not a fork. Reading commits...")
+      logger.info(f"Repo: {analysisConfig.ORG_NAME}/{i.get('name')} is not a fork. Reading commits...")
       repoData['isFork'].append(False)
       repoData['forkOf'].append(None)
 
     if i.get('fork') == False or analysisConfig.IGNORE_FORKS == False:
       commits_json = make_paged_request(REPO_COMMITS_URL, analysisConfig.NUM_COMMITS, analysisConfig, repo_name=i.get('name'))
-      commitStats = pd.concat([commitStats, commit_info(commits_json, False)], ignore_index=True)
+      commitDF, AICommitSummary = commit_info(commits_json, False, orgSummary, repoSummary)
+      commitStats = pd.concat([commitStats, commitDF], ignore_index=True)
+      repoData['AICommitSummary'].append(AICommitSummary)
+    else:
+      repoData['AICommitSummary'].append("None")
+
 
     if i.get('license') != None:
       repoData['license'].append(i.get('license').get('name'))
@@ -241,7 +314,7 @@ def aggRepo(json, analysisConfig: OrgAnalysisConfig):
     if count <= 0:
       break
 
-  logger.info(f"Done fetching repos for the {ANALYSIS_CONFIG.ORG_NAME} organization.\n")
+  logger.info(f"Done fetching repos for the {analysisConfig.ORG_NAME} organization.")
 
   forkedRepos = repoStats[repoStats['isFork'] == True]
   forkedRepos = forkedRepos.loc[:, ['name', 'forkOf']]
@@ -251,10 +324,9 @@ def aggRepo(json, analysisConfig: OrgAnalysisConfig):
 
 def repoOutput(repoStats, commitStats, analysisConfig: OrgAnalysisConfig):
   logger.info(f"Repos for {analysisConfig.ORG_NAME} GitHub analytics")
-  # if repoStats:
-  # display(HTML(repoStats.to_html(index=False))) TODO: output to CSV
 
   if repoStats.empty == False:
+    repoStats.to_json(f"{analysisConfig.OUTPUT_PATH}{analysisConfig.ORG_NAME}_repo_stats_{datetime.now().isoformat(timespec='seconds').replace(':', '-')}.json", index=True)
     create_histogram(repoStats["stars"], "Number of stars", "Frequency", f'{analysisConfig.ORG_NAME}: Histogram of stars', f"{analysisConfig.ORG_NAME}_stars", analysisConfig.OUTPUT_PATH)
     create_histogram(repoStats["watchers"], "Number of watchers", "Frequency", f'{analysisConfig.ORG_NAME}: Histogram of watchers', f"{analysisConfig.ORG_NAME}_watchers", analysisConfig.OUTPUT_PATH)
     create_histogram(repoStats["issues"], "Number of issues", "Frequency", f'{analysisConfig.ORG_NAME}: Histogram of issues', f"{analysisConfig.ORG_NAME}_issues", analysisConfig.OUTPUT_PATH)
@@ -265,6 +337,7 @@ def repoOutput(repoStats, commitStats, analysisConfig: OrgAnalysisConfig):
     create_pie(by_language, by_language.index, f"{analysisConfig.ORG_NAME} repos by language", f"{analysisConfig.ORG_NAME}_languages", analysisConfig.OUTPUT_PATH, analysisConfig.PIE_CHART_THRESHOLD)
 
   if commitStats.empty == False:
+    commitStats.to_json(f"{analysisConfig.OUTPUT_PATH}{analysisConfig.ORG_NAME}_commit_stats_{datetime.now().isoformat(timespec='seconds').replace(':', '-')}.json", index=True)
     commitData = commitStats.groupby("date").agg({"avatar_url": "count"}).reset_index()
     commitData.sort_values("date", inplace=True)
     commitData.rename(columns={'avatar_url': 'count'}, inplace=True)
@@ -277,19 +350,19 @@ def repoOutput(repoStats, commitStats, analysisConfig: OrgAnalysisConfig):
     create_pie(by_author["avatar_url"], author_label, f"{analysisConfig.ORG_NAME} commit authors for the last {analysisConfig.NUM_COMMITS} commits", f"{analysisConfig.ORG_NAME}_authors", analysisConfig.OUTPUT_PATH, analysisConfig.PIE_CHART_THRESHOLD)
 
 
-def repo_info(json, analysisConfig: OrgAnalysisConfig):
+def repo_info(json, analysisConfig: OrgAnalysisConfig, orgSummary: str):
   if json == None:
     logger.info(f"No repos found for {analysisConfig.ORG_NAME}. Exiting...")
     exit()
 
   # call repoStats
-  repoStats, commitStats, forkedRepos = aggRepo(json, analysisConfig)
+  repoStats, commitStats, forkedRepos = aggRepo(json, analysisConfig, orgSummary)
 
   if len(forkedRepos) > 0:
     logger.info(f"Forked repos for {analysisConfig.ORG_NAME}")
-    # display(HTML(forkedRepos.to_html(index=False))) TODO: output this to CSV
+    forkedRepos.to_csv(f"{analysisConfig.OUTPUT_PATH}{analysisConfig.ORG_NAME}_forked_repos_{datetime.now().isoformat(timespec='seconds').replace(':', '-')}.csv", index=True)
   else:
-    logger.info(f"There are no forked repos on the {analysisConfig.ORG_NAME} organization.\n")
+    logger.info(f"There are no forked repos on the {analysisConfig.ORG_NAME} organization.")
 
   if analysisConfig.IGNORE_FORKS:
     commitStats = commitStats[(commitStats["type"] != "Bot") & (commitStats["isFork"] == False)]
@@ -303,13 +376,30 @@ def repo_info(json, analysisConfig: OrgAnalysisConfig):
     logger.info(f"There are no commits on repos on the {analysisConfig.ORG_NAME} organization. Not generating graphs. Exiting... ")
     return
 
-  # TODO: generate image files to /ANALYSIS_CONFIG.ORG_NAME dir
   logger.info(f"Showing GitHub analytics for {analysisConfig.ORG_NAME}")
   repoOutput(repoStats, commitStats, analysisConfig)
 
-  
+  AI_repo_summaries = orgSummary
+  AI_repo_summaries += '\n'.join([str(x) for x in repoStats['AICommitSummary'] if pd.notna(x)])
+  logger.info(f"AI repo summaries: {AI_repo_summaries}")
+  logger.info(f"Input tokens: {AI_client.count_tokens(AI_repo_summaries)}")
+  message = AI_client.messages.create(
+    max_tokens=1024,
+    system=f"You are a software expert working for an investment firm. You are given summaries of a Github organization, and commits to its repositories. The investment firm wants to know two key things: if there could be any red flags for the organization, and if there are any opportunities for the organization. You are to summarize the information in a way that is easy to understand for a non-technical person. You are to focus on the opportunities and red flags. You are to be concise and to the point. Do not mention needed further evaluation or due dilligence needed as this is a given. Do not make unsubstantiated claims.",
+    messages=[
+      {
+        "role": "user",
+        "content": AI_repo_summaries,
+      }
+    ],
+    model="claude-3-opus-20240229",
+  )
+  # logger.info(f"AI response: {message.content}")
+  logger.info(f"AI response: {message.content[0].text}")
+  logger.info(f"AI response tokens: {AI_client.count_tokens(message.content[0].text)}")
+  AI_summary = message.content[0].text
 
-def org_members_info(j):
+def org_members_info(j, analysisConfig: OrgAnalysisConfig):
   if len(j) == 0:
     logger.info("No public org members.\n")
     return
@@ -332,27 +422,29 @@ def org_members_info(j):
     htmllist.append({"followers": user_json.get("followers"), "html": html})
 
   htmllist = sorted(htmllist, key=lambda x: x["followers"], reverse=True)
-  container = "<div style='width: 100%; display: flex; flex-wrap: wrap; justify-content: center; margin-top: 10px'>"
+  container = "<div style='width: 100%; display: flex; flex-wrap: wrap; justify-content: center; margin-top: 10px; font-family:\"Quicksand\"'>"
   for i in htmllist:
     container += i["html"]
   container += "</div>"
-  # display(HTML(container)) TODO: output this to file
+
+  with open(f"{analysisConfig.OUTPUT_PATH}{analysisConfig.ORG_NAME}_org_members_{datetime.now().isoformat(timespec='seconds').replace(':', '-')}.html", "w") as f:
+    f.write(container)
 
 def main(analysisConfig: OrgAnalysisConfig):
     logger.info(f"STARTED: GitHub organisation analytics for {analysisConfig.ORG_NAME}.")
     
     logger.info(f"1. Org info for {analysisConfig.ORG_NAME}...")
     org_json = make_request(ORG_URL.format(org_name=analysisConfig.ORG_NAME))
-    org_info(org_json)
+    org_summary = org_info(org_json, analysisConfig)
 
     # logger.info(f"2. Public org member info for {analysisConfig.ORG_NAME}...")
     # org_members_json = make_request(ORG_MEMBERS_URL.format(org_name=analysisConfig.ORG_NAME))
-    # org_members_info(org_members_json)
+    # org_members_info(org_members_json, analysisConfig)
 
     logger.info(f"3. Repo and commit info for: {analysisConfig.ORG_NAME}...")
     repo_json = make_paged_request(ORG_REPO_URL, org_json.get('public_repos'), analysisConfig)
     # pretty_json(repo_json)
-    repo_info(repo_json, analysisConfig)
+    repo_info(repo_json, analysisConfig, org_summary)
 
 if __name__ == "__main__":
   parser = argparse.ArgumentParser(
